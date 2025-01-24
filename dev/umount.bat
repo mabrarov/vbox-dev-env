604e1b85690cea57e537d5d4caa072d4e16dcbeb
Marat Abrarov <abrarov@gmail.com>
2025-01-24 19:10:17 +0300

    Fixed Windows script for unmounting virtual drives with persistent data.

--- dev/umount.bat
+++ dev/umount.bat
@@ -1,6 +1,6 @@
 @echo off
-set PATH="%ProgramFiles%\Oracle\VirtualBox;%PATH%"
+set "PATH=%ProgramFiles%\Oracle\VirtualBox;%PATH%"
 set VM_NAME=dev
-VBoxManage.exe storageattach %VM_NAME% --storagectl SATA --port 1 --device 0 --type hdd --medium none
-VBoxManage.exe storageattach %VM_NAME% --storagectl SATA --port 2 --device 0 --type hdd --medium none
-VBoxManage.exe storageattach %VM_NAME% --storagectl SATA --port 3 --device 0 --type hdd --medium none
+VBoxManage storageattach "%VM_NAME%" --storagectl SATA --port 1 --device 0 --type hdd --medium none
+VBoxManage storageattach "%VM_NAME%" --storagectl SATA --port 2 --device 0 --type hdd --medium none
+VBoxManage storageattach "%VM_NAME%" --storagectl SATA --port 3 --device 0 --type hdd --medium none
