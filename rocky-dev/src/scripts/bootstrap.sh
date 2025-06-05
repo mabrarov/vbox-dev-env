0187af09f99c4d44c7ac3c346fbd4e101db3f2c6
Marat Abrarov <abrarov@gmail.com>
2025-06-06 00:30:29 +0300

    Go SDK 1.24.3 -> 1.24.4

--- rocky-dev/src/scripts/bootstrap.sh
+++ rocky-dev/src/scripts/bootstrap.sh
@@ -67,7 +67,7 @@ gradle_home="${opt_bin_dir}/gradle"
 groovy_version="4.0.24"
 groovy_home="${opt_bin_dir}/groovy"
 
-golang_version="1.24.3"
+golang_version="1.24.4"
 golang_home="${opt_bin_dir}/go"
 
 docker_compose_version="2.32.4"
